613b8908e7f0c964c00a7f63cd684611a565f558
Penasta <toledis@gmail.com>
2021-09-17 00:58:29 -0300

    90% arrumado

--- r_docs/Exercicio 1.R
+++ r_docs/Exercicio 1.R
@@ -10,6 +10,9 @@ library(tidyverse)
 
 sim_consolidado <- readRDS("./data/SIM/sim_consolidado.rds")
 
+# Preparação do banco: Idade de código para número, remoção de variáveis que não vão ser utilizadas,
+# introdução de um ifelse comparativo para definir se a pessoa nasceu antes ou depois da metade exata do ano
+# para posterior plotagem, filtro para nascidos após 1999, conforme se pede nos exercícios, etc
 sim_lexis<-sim_consolidado %>% select(DTOBITO,DTNASC,CODMUNRES,IDADE) %>%
   mutate(DTOBITO=dmy(DTOBITO),
          DTNASC=dmy(DTNASC))
@@ -30,45 +33,48 @@ sim_lexis$ANOOBITO <- year(sim_lexis$DTOBITO)
 sim_lexis$ANONASC <- as.numeric(sim_lexis$ANONASC)
 sim_lexis$ANOOBITO <- as.numeric(sim_lexis$ANOOBITO)
 
-
-
 sim_lexis <- sim_lexis %>%
   filter (ANONASC > 1999)
 
 sim_lexis$comparativo <- ymd(paste(sim_lexis$ANONASC,"-06-01"))
-sim_lexis$TRI <- ifelse(sim_lexis$DTNASC <= sim_lexis$comparativo,"0","1")
+sim_lexis$TRI <- ifelse(sim_lexis$DTNASC <= sim_lexis$comparativo,"0","1")          
 sim_lexis$TRI <- as.numeric(sim_lexis$TRI)
 
-dados <- sim_lexis %>%
-  mutate(IDOBITO= as.numeric(difftime(DTOBITO, DTNASC, units = "days")/365),id_ob_anos_comp=floor(IDOBITO)) %>% 
-  group_by(ANONASC, ANOOBITO, TRI) %>%
+#Filtro Utilizado pois constavam pessoas nascidas >2000 com mais de 20 anos (alguns com mais de 70)
+sim_lexis <- sim_lexis %>%
+  filter (IDADEA < 21)
+
+as_tibble(sim_lexis)
+
+# Agrupando os dados: Essa parte que está dando pau
+# Provavelmente arrumado (?)
+dados2 <- sim_lexis %>%
+  mutate(
+    IDOBITO= as.numeric(difftime(DTOBITO, DTNASC, units = "days")/365),
+    ano_nasc=year(DTNASC),
+    ano_obito=year(DTOBITO)) %>%
+  group_by(ano_nasc, ano_obito, TRI) %>%
   mutate(quantidade=n()) %>%
-  ungroup() %>% 
-  distinct(ANONASC, ANOOBITO, TRI,.keep_all = T) %>%
-  select(ANONASC,ANOOBITO, quantidade, TRI,id_ob_anos_comp)
+  ungroup() %>%
+  distinct(ano_nasc, ano_obito, TRI, .keep_all = T) %>%
+  select(ano_nasc,ano_obito, quantidade, TRI) %>%
+  arrange(ano_obito, desc(ano_nasc))
 
-#QUESTÃO 1) 
-# a)
-### Este diagrama ainda apresenta erros. (os números vêm empilhados em cada triângulo ???)
-### Esta questão pede para trabalhar com os dados do SINASC, mas o mesmo não vêm com dados de mortalidade,
-### portanto, optei por utilizar os dados do SIM.
+dados2$id_ob_anos_comp <- (dados2$ano_obito - dados2$ano_nasc)
 
-
+## Lexis Plot (Dados fora de posição[ex o que deveria estar na coorte 2000, está na coorte 1999])
 diagrama <- lexis_grid(year_start=2000,year_end=2020,age_start=0,age_end=5,delta=1)
 
+
 diagrama <- diagrama +
-  annotate(geom="text", x=as.Date(paste0(dados$ANOOBITO[dados$TRI==0]
+  annotate(geom="text", x=as.Date(paste0(dados2$ano_obito[dados2$TRI==0]
                                          ,"-08-06"))
-           ,y=dados$id_ob_anos_comp[dados$TRI==0]+0.3,
-           label=c(paste0(dados$quantidade[dados$TRI==0])),
+           ,y=dados2$id_ob_anos_comp[dados2$TRI==0]+0.3,
+           label=c(paste0(dados2$quantidade[dados2$TRI==0])),
            color="red") +
-  annotate(geom="text", x=as.Date(paste0(dados$ANOOBITO[dados$TRI==1]
+  annotate(geom="text", x=as.Date(paste0(dados2$ano_obito[dados2$TRI==1]
                                          ,"-05-06"))
-           ,y=dados$id_ob_anos_comp[dados$TRI==1]+0.75,
-           label=c(paste0(dados$quantidade[dados$TRI==1])),
+           ,y=dados2$id_ob_anos_comp[dados2$TRI==1]+0.75,
+           label=c(paste0(dados2$quantidade[dados2$TRI==1])),
            color="blue")
 diagrama
-
-# Segue [muito] bugado
-# Parece que, além de empilhar, os dados estão entrando na coorte errada (por exemplo, a obs. "207" que aparece no canto inferior esquerdo, deveria estar
-# no losando a direita, indicando que nasceu em 2000 e morreu em 2000, porém aparece como nascido em 1999)
