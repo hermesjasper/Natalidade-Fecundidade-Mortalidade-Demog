c867e2f37837b6bb2082a0eb5892df08c0fc0dd9
analalanja <ana.luiza.ca@hotmail.com>
2021-09-29 20:37:05 -0300

    butei o meu

--- aux-source.R
+++ aux-source.R
@@ -1,5 +1,6 @@
 sim_consolidado <- readRDS("./data/SIM/sim_consolidado.rds")
 sinasc_consolidado <- readRDS("./data/sinasc/sinasc_consolidado.rds")
 
-source(here::here('r_docs/Exercicio 1.R'), encoding = 'UTF-8')
+source(here::here('r_docs/Exercicio 1.R'))
+source(here::here('r_docs/Questão 2.R'), encoding = 'UTF-8')
 source(here::here('r_docs/3b-c.R'), encoding = 'UTF-8')
